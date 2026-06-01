a6e900d4ffb33674adf8ec0ba32a2ef30192c3b3
Dyakonov <aectan1987@ya.ru>
2026-06-01 18:09:59 +0300

    fix WdHubStatusTests

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -3,12 +3,10 @@ package tests;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeAll;
 
-
 public class TestBase {
-    protected static final String basePath = "/wd/hub/status";
-
     @BeforeAll
     public static void setUp() {
         RestAssured.baseURI = "https://selenoid.autotests.cloud";
+        RestAssured.basePath = "/wd/hub";
     }
 }
